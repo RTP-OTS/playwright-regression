6f823b596253139724ea490fda54354ce4508a74
RattaponOudtagosum <rattapon.ot@gmail.com>
2024-01-30 18:23:47 +0700

    update

--- tests/AutomateWeb/tc04.spec.js
+++ tests/AutomateWeb/tc04.spec.js
@@ -34,4 +34,41 @@ test ('Verify - shop' , async ({page}) =>{
     console.log(await itemIncart.textContent());
     await expect(itemIncart).toContainText(productName)
 
-});
+    await page.locator("div li").first().waitFor();
+    const bool = await page.locator("h3:has-text('ADIDAS ORIGINAL')").isVisible();
+    expect(bool).toBeTruthy();
+    await page.locator("text=Checkout").click();
+  
+    await page.locator("[placeholder*='Country']").type("ind");
+  
+    const dropdown = page.locator(".ta-results");
+    await dropdown.waitFor();
+    const optionsCount = await dropdown.locator("button").count();
+    for (let i = 0; i < optionsCount; ++i) {
+       const text = await dropdown.locator("button").nth(i).textContent();
+       if (text === " India") {
+          await dropdown.locator("button").nth(i).click();
+          break;
+       }
+    }
+  
+    expect(page.locator(".user__name [type='text']").first()).toHaveText(username);
+    await page.locator(".action__submit").click();
+    await expect(page.locator(".hero-primary")).toHaveText(" Thankyou for the order. ");
+    const orderId = await page.locator(".em-spacer-1 .ng-star-inserted").textContent();
+    console.log(orderId);
+  
+    await page.locator("button[routerlink*='myorders']").click();
+    await page.locator("tbody").waitFor();
+    const rows = await page.locator("tbody tr");
+    for (let i = 0; i < await rows.count(); ++i) {
+       const rowOrderId = await rows.nth(i).locator("th").textContent();
+       if (orderId.includes(rowOrderId)) {
+          await rows.nth(i).locator("button").first().click();
+          break;
+       }
+    }
+    const orderIdDetails = await page.locator(".col-text").textContent();
+    expect(orderId.includes(orderIdDetails)).toBeTruthy();
+  
+ });
