24d85106616a9a212d0d601f80db04c49ab73153
RattaponOudtagosum <rattapon.ot@gmail.com>
2024-02-06 18:05:06 +0700

    update

--- tests/AutomateWeb/tc07.spec.js
+++ tests/AutomateWeb/tc07.spec.js
@@ -1,10 +1,13 @@
-const {test,expect} = require('@playwright/test');
-const {LoginPage} = require('../../pageobject/LoginPage');
-const JSONdata = require('../../page/config.json')
+const { test,expect } = require('@playwright/test');
+const { LoginPage } = require('../../pageobject/loginPage_web')
+const JSONdata = require('../../page/datatest.json');
+const { DashboardPage } = require('../../pageobject/dashboard_web');
 
 test ('test app' , async ({page})=> {
     const loginPage = new LoginPage(page);
     await loginPage.goTo()
-    await loginPage.validLogin(JSONdata.user.username,
-        JSONdata.user.password)
+    await loginPage.validLogin(JSONdata.user.username,JSONdata.user.password)
+    const dashboardPage = new DashboardPage(page);
+    await dashboardPage.searchProductAddCart(JSONdata.productName)
+    await dashboardPage.navigateToCart();
 });
